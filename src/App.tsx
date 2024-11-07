5fbe8f460a331f8112f27cb6e16dc0452fe66e25
Bruna <brunscandido@gmail.com>
2024-11-07 10:58:06 +0100

    fix: install missing packages

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './styles/App.scss';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <h1>Welcome to My Portfolio</h1>
+    </div>
+  );
+}
+
+export default App;
